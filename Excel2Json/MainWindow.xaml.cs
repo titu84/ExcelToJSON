4127a1894c7934777d697ea919a5fc8937f7395a
titu84 <krzysztof.sauermann@gmail.com>
2018-04-14 19:55:54 +0200

    .

---
+++ Excel2Json/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Win32;
+using ReadDataFromExcel;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Web.Script.Serialization;
+
+namespace Excel2Json
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        string patch;
+        string json;
+        private void btnGetExcel_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Excel|*.xls;*.xlsx;*.xlsm";
+            if (openFileDialog.ShowDialog() == true)
+                patch = openFileDialog.FileName;
+            tbFileName.Text = patch;
+            btnCreateJson.IsEnabled = true;
+            btnSaveJson.IsEnabled = false;
+        }
+
+        private void btnCreateJson_Click(object sender, RoutedEventArgs e)
+        {
+            btnCreateJson.IsEnabled = false;
+            ExcelReader gef = new ExcelReader(patch);
+            json = gef.GetJsonFormExcel(1);
+            richTextBox.Document.Blocks.Clear();
+            richTextBox.Document.Blocks.Add(new Paragraph(new Run(json)));
+            richTextBox.IsEnabled = false;            
+            btnSaveJson.IsEnabled = true;
+        }
+
+        private void btnSaveJson_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "JSON file (*.json)|*.json";
+            if (saveFileDialog.ShowDialog() == true)
+                File.WriteAllText(saveFileDialog.FileName, json);
+            MessageBoxResult result = MessageBox.Show("Zapisano");
+            
+        }
+    }
+}
---
+++ ReadDataFromExcel/ExcelReader.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Web.Script.Serialization;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ReadDataFromExcel
+{
+    public class ExcelReader
+    {
+        string patch;
+        Excel.Application xlApp;
+        Excel.Workbook xlWorkbook;
+        Excel._Worksheet xlWorksheet;
+        Excel.Range xlRange;
+        public ExcelReader(string _patch)
+        {
+            patch = _patch;
+        }
+        public string GetJsonFormExcel(int sheetID = 1)
+        {
+            try
+            {
+                StringBuilder s = new StringBuilder();
+                List<object[]> lo = new List<object[]>();
+                xlApp = new Excel.Application();
+                xlWorkbook = xlApp.Workbooks.Open(patch);
+                xlWorksheet = xlWorkbook.Sheets[sheetID];
+                xlRange = xlWorksheet.UsedRange;
+                int rowCount = xlRange.Rows.Count;
+                int colCount = xlRange.Columns.Count;
+                for (int i = 1; i <= rowCount; i++)
+                {
+                    object[] o = new object[colCount];
+                    for (int j = 1; j <= colCount; j++)
+                    {
+                        if (xlRange.Cells[i, j] != null && xlRange.Cells[i, j].Value2 != null)
+                            o[j - 1] = xlRange.Cells[i, j].Value2;
+                    }
+                    lo.Add(o);
+                }
+                return new JavaScriptSerializer().Serialize(lo);
+            }
+            catch (NullReferenceException nex)
+            {
+                return nex.Message;
+            }
+            catch (COMException ex)
+            {
+                return ex.Message;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+            finally
+            {
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+                try
+                {
+                    Marshal.ReleaseComObject(xlRange);
+                }
+                catch { }
+                try
+                {
+                    Marshal.ReleaseComObject(xlWorksheet);
+                    xlWorkbook.Close(false);
+                }
+                catch { }
+                try
+                {
+                    Marshal.ReleaseComObject(xlWorkbook);
+                }
+                catch { }
+                try
+                {
+                    xlApp.Quit();
+                    Marshal.ReleaseComObject(xlApp);
+                }
+                catch { }
+            }
+        }
+    }
+}
